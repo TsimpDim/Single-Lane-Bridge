8ac033f47731e700a2f3ac407e915fc36ffca04d
TsimpDim <dimitristsimpitas@gmail.com>
2019-02-27 14:30:37 +0200

    Allow collisions

--- src/com/tsimpdim/Car.java
+++ src/com/tsimpdim/Car.java
@@ -14,17 +14,11 @@ public class Car extends Thread{
     @Override
     public void run() {
 
+
         if(bridge.isBeingCrossed())
-            System.out.println(name + " is waiting to cross the bridge");
+            System.out.println("<!> Collision is about to happen");
 
-        while(bridge.isBeingCrossed()){
-            try{
-                Thread.sleep(5);
-            } catch (InterruptedException e) {}
-        }
-
-        // After the other car is done crossing the bridge, cross it yourself
-        bridge.lock();
+        bridge.lock(); // Lock here doesn't actually do anything other than notify the bridge that a car is crossing
 
         System.out.println(name + " is crossing the bridge");
 
@@ -36,5 +30,6 @@ public class Car extends Thread{
         System.out.println(name + " crossed the bridge");
 
         bridge.unlock();
+
     }
 }
